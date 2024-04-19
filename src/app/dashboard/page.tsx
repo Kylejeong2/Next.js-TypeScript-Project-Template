8f3c3b31eab57e9abdbe312345a1e3bdf7f2854c
Kylejeong2 <Kylejeong21@gmail.com>
2024-04-19 11:35:26 -0700

    update jsx and content

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import CreateNoteDialog from '@/components/CreateNoteDialog';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { db } from '@/lib/db';
@@ -29,7 +28,7 @@ const DashboardPage = async (props: Props) => {
                             </Link>
 
                             <div className='w-4'></div>
-                            <h1 className='text-3xl font-bold text-gray-900'>My Notes</h1>
+                            <h1 className='text-3xl font-bold text-gray-900'>Title</h1>
                             <div className='w-4'></div>
                             <UserButton />
                         </div>
@@ -39,40 +38,6 @@ const DashboardPage = async (props: Props) => {
 
                     <Separator />
                     
-                    <div className="h-8"></div>
-                    {/* <!-- conditional rendering --> */}
-                    {notes.length === 0 && (
-                        <div className='text-center'>
-                            <h2 className='text-xl text-gray-500'>You don't have any notes yet!</h2>
-                        </div>
-                    )}
-                    
-
-                    <div className='grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-3'>
-                        <CreateNoteDialog />
-                        {notes.map(note => {
-                            return (
-                                <a href={`/notebook/${note.id}`} key={note.id}>
-                                    <div className='border border-stone-200 rounded-lg overflow-hidden flex flex-col hover:shadow-xl transition hover:-translate-y-1'>
-                                        <img 
-                                            width={400}
-                                            height={200}
-                                            alt={note.name}
-                                            src={note.imageUrl || ""}
-                                        />
-                                    <div className='p-4'>
-                                        <h3 className='text-xl font-semibold text-gray-900'>{note.name}</h3>
-                                        <div className='h-1'></div>
-                                        <p className='text-sm text-gray-500'>
-                                            {new Date(note.createdAt).toLocaleDateString()}
-                                        </p>
-                                    </div>
-                                    </div>
-                                    
-                                </a>
-                            )
-                        })}
-                    </div>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import TypewriterTitle from '@/components/TypewriterTitle';
 import { Button } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react';
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link';
 
 export default function Home() {
@@ -9,12 +8,11 @@ export default function Home() {
     <div className='bg-gradient-to-r min-h-screen grainy from rose-100 to-teal-100'>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         <h1 className='font-semibold text-7xl text-center'>
-          AI <span className='text-green-600 font-bold'>Note Taking</span> Assistant
+            PROJECT TITLE // INTRO PAGE
         </h1>
 
         <div className='mt-4'>
           <h2 className='font-semibold text-3xl text-center text-slate-700'>
-            <TypewriterTitle />
           </h2> 
 
           <div className='mt-8'></div>
